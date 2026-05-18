aa8f1fec505eb30ad6c78a4a60a3b78ad2ef6ae7
Krish Ganvit <24cp001@bvmengineering.ac.in>
2026-05-18 15:26:44 +0530

    splash screen completed

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,23 +1,6 @@
-import'package:http/http.dart' as http;
-void main()async {
- 
-  var url = Uri.parse('https://jsonplaceholder.typicode.com/users');
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Widgets/splash.dart';
 
-  // Correct HTTP request
-  var response = await http.get(url);
-
-  print('Response status: ${response.statusCode}');
-  print('Response body: ${response.body}');
-  // runApp(
-  //   MaterialApp(
-  //     home: Scaffold(
-  //       appBar: AppBar(
-  //         backgroundColor: const Color.fromARGB(255, 162, 75, 255),
-  //         foregroundColor: Colors.white,
-  //         title: Center(child: Text("Profile")),
-  //       ),
-  //       body: Center(child: MyWidget()),
-  //     ),
-  //   ),
-  // );
+void main() async {
+  runApp(MaterialApp(home: (Splash()), debugShowCheckedModeBanner: false));
 }
